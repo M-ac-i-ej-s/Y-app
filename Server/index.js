5b2c151f7f6e733d71e57bf68876bbcd0b40f790
M-ac-i-ej-s <maciej.slupianekge@gmail.com>
2024-01-18 19:08:23 +0100

    todo-upd

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,6 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import multer from 'multer';
 
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
